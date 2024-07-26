45939efb01571701b87c5a1af59fcc1723b6a73a
tpositive <trposivio@gmail.com>
2024-07-26 08:38:57 -0600

    Added to the project by allowing users to search for specific words in the text

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import string
 
 def main():
+    user_input = False
+    while user_input == False:
+        question1 = input("Do you want to search for specific words? (y/n): ")
+        if question1 == 'y' or question1 == 'Y':
+            user_keywords = input("Enter the word(s) you would like to search seperated by ',' if you would like to search multiple words:\n").split(',')
+            user_input = True
+        elif question1 == 'n' or question1 == 'N':
+            user_keywords = ''
+            user_input =True
+        else:
+            print("Please enter y or n")
     with open("books/frankenstein.txt") as file:
         contents = file.read()
-        count = word_count(contents)
+        count,keyword_count = word_count(contents, user_keywords)
         letters = letter_count(contents)
         letter_list = []
         for letter in letters:
             letter_list.append({"letter": letter, "count": letters[letter]})
         letter_list.sort(reverse=True, key=dictionary_sort)
+        if len(keyword_count) > 0:
+            keyword_count.sort(reverse=True, key=dictionary_sort)
         print("--- Begin report of books/frankenstein.txt ---")
-        print(f"{count}\n")
+        print(f"Word Count: {count}\n")
+        if user_keywords:
+            print(f"User provided key words:\n {user_keywords}")
+            for dictionary in keyword_count:
+                print(f"The word '{dictionary['word']}' was found {dictionary['count']} in the text")
+
         for dictionary in letter_list:
             print(f"The '{dictionary['letter']}' character was found {dictionary['count']} times")
         print("--- End report ---")
 
-def word_count(text):
+def word_count(text, keywords=[]):
     words = text.split()
     count = len(words)
-    return count
+    new_keyword_list = []
+    ##optional keyword search##
+    if len(keywords) > 0:
+        for item in keywords:
+            item = str(item).lower()
+            keyword_count = 0
+            for word in words:
+                if word == item:
+                    keyword_count += 1
+            new_keyword_list.append({'word': item, 'count': keyword_count})
+    return count, new_keyword_list
 
 def letter_count(text):
     text_lowered = text.lower()
